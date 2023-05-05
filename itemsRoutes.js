4c37784cd64f8f7c4dd68085918b72ddef880292
Kelsey Guffanti <kc.breen3@gmail.com>
2023-05-05 11:50:11 -0400

    working routes

---
+++ itemsRoutes.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = new express.Router();
+const ExpressError = require("./ExpressError");
+const items = require("./fakeDB");
+
+router.get("/", function (req, res) {
+  res.json({ items });
+});
+
+router.post("/", function (req, res) {
+  const newItem = { name: req.body.name, price: req.body.price };
+  items.push(newItem);
+  res.status(201).json({ added: newItem });
+});
+
+router.get("/:name", function (req, res) {
+  let findName = req.params.name;
+  const foundItem = items.find((x) => x.name === findName);
+  if (foundItem === undefined) {
+    throw new ExpressError("Item not found", 404);
+  }
+  res.json({ foundItem });
+});
+
+router.patch("/:name", function (req, res) {
+  let findName = req.params.name;
+  const foundItem = items.find((x) => x.name === findName);
+  if (foundItem === undefined) {
+    throw new ExpressError("Item not found", 404);
+  }
+  foundItem.name = req.body.name;
+  res.json({ updated: foundItem });
+});
+
+router.delete("/:name", function (req, res) {
+  let findName = req.params.name;
+  const foundItem = items.find((x) => x.name === findName);
+  items.splice(foundItem, 1);
+  res.json({ message: "Deleted" });
+});
+
+module.exports = router;
